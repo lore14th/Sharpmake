74aca8b63214da6d8d658fecdf223084d98fcdc4
Lorenzo Fortina <lorenzo.fortina.95@gmail.com>
2026-03-29 13:43:42 +0200

    Adapt to new API

--- Sharpmake.Build.cs
+++ Sharpmake.Build.cs
@@ -9,7 +9,7 @@ using TinfoilBuildTool;
 
 public class SharpmakeBaseProject : DotNetProject
 {
-    public SharpmakeBaseProject(string projectGuid)
+    public SharpmakeBaseProject(string projectName, string projectGuid) : base(projectName)
     {
         m_ProjectGuid = projectGuid;
     }
@@ -35,9 +35,8 @@ public class SharpmakeBaseProject : DotNetProject
 [Sharpmake.Compile]
 public class SharpmakeApplicationProject : SharpmakeBaseProject
 {
-    public SharpmakeApplicationProject() : base(@"37CF3EE3-AFD3-3CC8-8F8E-B423292D491F")
+    public SharpmakeApplicationProject() : base("Sharpmake.Application", @"37CF3EE3-AFD3-3CC8-8F8E-B423292D491F")
     {
-        Name = "Sharpmake.Application";
         SourceRootPath = @"[project.SharpmakeCsPath]/Sharpmake.Application";
     }
 
@@ -55,9 +54,8 @@ public class SharpmakeApplicationProject : SharpmakeBaseProject
 [Sharpmake.Compile]
 public class SharpmakeCoreProject : SharpmakeBaseProject
 {
-    public SharpmakeCoreProject() : base(@"15F793C7-9E88-64A9-591C-7244FCC6B771")
+    public SharpmakeCoreProject() : base("Sharpmake", @"15F793C7-9E88-64A9-591C-7244FCC6B771")
     {
-        Name = "Sharpmake";
         SourceRootPath = @"[project.SharpmakeCsPath]/Sharpmake";
     }
 
@@ -70,9 +68,8 @@ public class SharpmakeCoreProject : SharpmakeBaseProject
 [Sharpmake.Compile]
 public class SharpmakeFunctionalTestsProject : SharpmakeBaseProject
 {
-    public SharpmakeFunctionalTestsProject() : base(@"E659DEAC-D040-459F-9E6C-E694AF802DBC")
+    public SharpmakeFunctionalTestsProject() : base("Sharpmake.FunctionalTests", @"E659DEAC-D040-459F-9E6C-E694AF802DBC")
     {
-        Name = "Sharpmake.FunctionalTests";
         SourceRootPath = @"[project.SharpmakeCsPath]/Sharpmake.FunctionalTests";
     }
 
@@ -87,9 +84,8 @@ public class SharpmakeFunctionalTestsProject : SharpmakeBaseProject
 [Sharpmake.Compile]
 public class SharpmakeGeneratorsProject : SharpmakeBaseProject
 {
-    public SharpmakeGeneratorsProject() : base(@"844F66DE-B015-340E-720A-8E158B517E93")
+    public SharpmakeGeneratorsProject() : base("Sharpmake.Generators", @"844F66DE-B015-340E-720A-8E158B517E93")
     {
-        Name = "Sharpmake.Generators";
         SourceRootPath = @"[project.SharpmakeCsPath]/Sharpmake.Generators";
     }
 
@@ -104,9 +100,8 @@ public class SharpmakeGeneratorsProject : SharpmakeBaseProject
 [Sharpmake.Compile]
 public class SharpmakeCommonPlatformsProject : SharpmakeBaseProject
 {
-    public SharpmakeCommonPlatformsProject() : base(@"6CA349ED-D54F-E547-E2AC-E11BEF4F21CF")
+    public SharpmakeCommonPlatformsProject() : base("Sharpmake.CommonPlatforms", @"6CA349ED-D54F-E547-E2AC-E11BEF4F21CF")
     {
-        Name = "Sharpmake.CommonPlatforms";
         SourceRootPath = @"[project.SharpmakeCsPath]/Sharpmake.Platforms/Sharpmake.CommonPlatforms";
     }
 
@@ -122,9 +117,8 @@ public class SharpmakeCommonPlatformsProject : SharpmakeBaseProject
 [Sharpmake.Compile]
 public class SharpmakeSamplesProject : SharpmakeBaseProject
 {
-    public SharpmakeSamplesProject() : base(@"60452228-D411-4461-91D7-0894482D940C")
+    public SharpmakeSamplesProject() : base("Samples", @"60452228-D411-4461-91D7-0894482D940C")
     {
-        Name = "Samples";
         SourceRootPath = @"[project.SharpmakeCsPath]/Samples";
     }
 
@@ -139,9 +133,8 @@ public class SharpmakeSamplesProject : SharpmakeBaseProject
 [Sharpmake.Compile]
 public class SharpmakeUnitTestsProject : SharpmakeBaseProject
 {
-    public SharpmakeUnitTestsProject() : base(@"3776B212-14C5-4038-9B97-2AACB2D10987")
+    public SharpmakeUnitTestsProject() : base("Sharpmake.UnitTests", @"3776B212-14C5-4038-9B97-2AACB2D10987")
     {
-        Name = "Sharpmake.UnitTests";
         SourceRootPath = @"[project.SharpmakeCsPath]/Sharpmake.UnitTests";
     }
 
